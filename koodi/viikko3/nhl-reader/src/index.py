d45a0f7c82561c630d7f13e58e2de77d7aa7f629
Topias Ukkonen <ukkonentopias@gmail.com>
2022-11-12 17:08:03 +0200

    nhl task

--- koodi/viikko3/nhl-reader/src/index.py
+++ koodi/viikko3/nhl-reader/src/index.py
@@ -4,22 +4,19 @@ import requests
 def main():
     url = "https://nhlstatisticsforohtu.herokuapp.com/players"
     response = requests.get(url).json()
-
-    print("JSON-muotoinen vastaus:")
-    print(response)
+    #print("JSON-muotoinen vastaus:")
+    #print(response)
 
     players = []
 
     for player_dict in response:
-        player = Player(
-            player_dict['name']
-        )
-
-        players.append(player)
-
-    print("Oliot:")
-
-    for player in players:
-        print(player)
+        if player_dict["nationality"] == "FIN":
+            players.append(player_dict)
+    sort = sorted(players, key=lambda player: player['goals'] + player['assists'] , reverse=True)
+    #print("Oliot:")
+    print("Players from FIN:")
+    for player in sort:
+        print(f"{player['name']} {player['goals']} + {player['assists']} = {player['goals'] + player['assists']}")
+        
 if __name__ == "__main__":
     main()
